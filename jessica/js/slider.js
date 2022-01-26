1eac9753f0059888c97471a37b9c26404536f61b
Jéssica Pedro <jessicapedro165@gmail.com>
2022-01-26 07:39:04 +0000

    a

--- jessica/js/slider.js
+++ jessica/js/slider.js
@@ -5,21 +5,21 @@ window.onload = function () {
 /* let intrevalo=3000; */
 
 function slide1() {
-    document.getElementById("banner").src = "../../img/arcade/arcadeBanner1.png";
+    document.getElementById("banner").src = "img/arcade/arcadeBanner1.png";
     setTimeout("slide2()", 3000);
 }
 
 function slide2() {
-    document.getElementById("banner").src = "../../img/arcade/iniciomodo.jpg";
+    document.getElementById("banner").src = "img/arcade/iniciomodo.jpg";
     setTimeout("slide3()", 3000);
 }
 
 function slide3() {
-    document.getElementById("banner").src = "../../img/arcade/tipos.jpg";
+    document.getElementById("banner").src = "img/arcade/tipos.jpg";
     setTimeout("slide4()", 3000);
 }
 
 function slide4() {
-    document.getElementById("banner").src = "../../img/arcade/mapas.jpg";
+    document.getElementById("banner").src = "img/arcade/mapas.jpg";
     setTimeout("slide1()", 3000);
 }
